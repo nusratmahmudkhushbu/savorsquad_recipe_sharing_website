d162d5d06366900ba5b29ab1f3df5dd13d4da6fd
Anika Bushra <anika.bushra4@g.bracu.ac.bd>
2024-12-24 14:22:01 +0000

    initial update

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -1,13 +1,14 @@
 export default function Hero() {
-  return (
-    <section className="relative overflow-hidden">
-      <div className="relative w-screen h-auto">
-        <img
-          className="w-screen h-auto object-cover"
-          src="./Screenshot 2024-12-03 133925.png"
-          alt="SavorySwap"
-        />
-      </div>
-    </section>
-  );
-}
+    return (
+      <section className="relative overflow-hidden">
+        <div className="relative w-screen h-auto">
+          <img
+            className="w-screen h-auto object-cover"
+            src="./Screenshot 2024-12-03 133925.png"
+            alt="SavorySwap"
+          />
+        </div>
+      </section>
+    );
+  }
+  
